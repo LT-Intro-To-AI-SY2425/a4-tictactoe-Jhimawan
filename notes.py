095f0e7dbdcfa948c0bb7531c4c1f017ca773c23
Jhimawan <Jhimawan@cps.edu>
2024-10-30 11:20:18 -0500

    dog class

--- notes.py
+++ notes.py
@@ -2,3 +2,10 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self):
+        
+#instances of the Dog class
+logan = Dog()
+cookie = Dog()
+maple = Dog()
