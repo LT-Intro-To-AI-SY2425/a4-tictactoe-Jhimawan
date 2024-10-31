481a22116c7ab27a02ebb4a2ca0b94f6ea3ace1d
Jhimawan <Jhimawan@cps.edu>
2024-10-31 11:02:35 -0500

    notes

--- notes.py
+++ notes.py
@@ -6,16 +6,23 @@ class Dog:
     def __init__(self, n = "no name", a = 0):
         self.name = n
         self.age = a
+        self.fetch_count = 0
     
     def __str__(self) -> str:
         s = f"{self.name} is {self.age} years old"
         return s
+    
+    def play_fetch(self, num_fetch):
+        self.fetch_count += num_fetch
+    
+    def reset_fetch(self):
+        self.fetch_count = 0
 
 #instances of the Dog class
 logan = Dog("logan", 8)
 cookie = Dog("cookie", 8)
 maple = Dog("maple", 1)
 
-print(logan)
+print(logan.fetch_count)
 print(cookie)
 print(maple)
